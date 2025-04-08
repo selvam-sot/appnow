000698159fae76dafff828e9523bdf3a58f3eedd
selvam-sot <selvam@softwareoftomorrow.com>
2025-04-09 01:33:22 +0530

    added monthly service route

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -7,8 +7,8 @@ import vendorRoutes from './vendor.routes';
 import vendorServiceRoutes from './vendor-service.routes';
 import vendorServiceSlotRoutes from './vendor-service-slot.routes';
 import appointmentRoutes from './appointment.routes';
-// import manageAppointmentRoutes from './manage-appointment.routes';
-// import monthlyServiceRoutes from './monthly-service.routes';
+import manageAppointmentRoutes from './manage-appointment.routes';
+import monthlyServiceRoutes from './monthly-service.routes';
 
 const router = Router();
 
@@ -18,8 +18,8 @@ router.use('/vendors', vendorRoutes);
 router.use('/vendor-services', vendorServiceRoutes);
 router.use('/vendor-service-slots', vendorServiceSlotRoutes);
 router.use('/appointments', appointmentRoutes);
-// router.use('/manage-appointments', manageAppointmentRoutes);
-// router.use('/monthly-service', monthlyServiceRoutes);
+router.use('/manage-appointments', manageAppointmentRoutes);
+router.use('/monthly-service', monthlyServiceRoutes);
 
 // router.use('/users', userRoutes);
 
