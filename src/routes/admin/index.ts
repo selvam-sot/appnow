b9a3b52521a3f319c8031e803eef573f52f21a0e
selvam-sot <selvam@softwareoftomorrow.com>
2025-04-08 18:35:19 +0530

    fixing route not found issue

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,26 +1,26 @@
 import { Router } from 'express';
 
-import userRoutes from './user.routes';
+//import userRoutes from './user.routes';
 import categoryRoutes from './category.routes';
 import subCategoryRoutes from './sub-category.routes';
-import vendorRoutes from './vendor.routes';
-import vendorServiceRoutes from './vendor-service.routes';
-import vendorServiceSlotRoutes from './vendor-service-slot.routes';
-import appointmentRoutes from './appointment.routes';
-import manageAppointmentRoutes from './manage-appointment.routes';
-import monthlyServiceRoutes from './monthly-service.routes';
+// import vendorRoutes from './vendor.routes';
+// import vendorServiceRoutes from './vendor-service.routes';
+// import vendorServiceSlotRoutes from './vendor-service-slot.routes';
+// import appointmentRoutes from './appointment.routes';
+// import manageAppointmentRoutes from './manage-appointment.routes';
+// import monthlyServiceRoutes from './monthly-service.routes';
 
 const router = Router();
 
 router.use('/categories', categoryRoutes);
 router.use('/sub-categories', subCategoryRoutes);
-router.use('/vendors', vendorRoutes);
-router.use('/vendor-services', vendorServiceRoutes);
-router.use('/vendor-service-slots', vendorServiceSlotRoutes);
-router.use('/appointments', appointmentRoutes);
-router.use('/manage-appointments', manageAppointmentRoutes);
-router.use('/monthly-service', monthlyServiceRoutes);
+// router.use('/vendors', vendorRoutes);
+// router.use('/vendor-services', vendorServiceRoutes);
+// router.use('/vendor-service-slots', vendorServiceSlotRoutes);
+// router.use('/appointments', appointmentRoutes);
+// router.use('/manage-appointments', manageAppointmentRoutes);
+// router.use('/monthly-service', monthlyServiceRoutes);
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 
 export default router;
