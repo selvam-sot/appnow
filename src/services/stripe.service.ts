240875982a717af832ec6002732fba88698dcacd
selvam4win <panneerselvam28@gmail.com>
2025-09-30 01:36:02 +0530

    Fixed stripe secret key issue

--- src/services/stripe.service.ts
+++ src/services/stripe.service.ts
@@ -1,9 +1,7 @@
 import Stripe from 'stripe';
 import logger from '../config/logger';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-05-28.basil', // Fixed to match TypeScript types
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export interface PaymentIntentData {
     amount: number; // in cents
